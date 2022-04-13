91923d56cc9309f3c62904ce9f38f67c2bd45cc6
huynhvinh8398 <huynhvanvinh080398@gmail.com>
2022-04-13 08:23:14 +0700

    java

---
+++ src/md2/SystemTime.java
@@ -0,0 +1,11 @@
+package md2;
+import java.util.Date;
+
+public class SystemTime {
+    public static void main (String[] args){     // hàm main (sử dụng tắt gõ main + phím Tab)
+        Date now = new Date() ;  // Lấy ra thời gian hiện tại
+        System.out.println("Now is:" +now);   //sử dụng gõ tắt sout + Phím Tab
+}
+
+    }
+
---
+++ src/md2/variabledataType.java
@@ -0,0 +1,16 @@
+package md2;
+public class variabledataType {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10
+        float f = 20.5f; // khai báo biến f có kiểu là float có giá trị là 20.5}
+        double d = 20.5; // khai báo biến d có kiểu double có giá trị là 20.5
+       boolean b = true;  // khai báo biến b có kiểu là boolean có giá trị là true
+        char C = 'a';  // khai báo biến C có kiểu là char có giá trị là kí tự a
+        String s = "Hà Nội"; // khai báo biến s có kiểu là String có giá trị là chuỗi "Hà Nội"
+        System.out.println("i="+i);
+        System.out.println("f ="+f);
+        System.out.println("d ="+d);
+        System.out.println("b ="+b);
+        System.out.println("s = " +s);
+}
+}
