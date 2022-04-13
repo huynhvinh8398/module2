07b9b5bdfbde1bc1d2661aea3b7ac4a36e48e917
huynhvinh8398 <huynhvanvinh080398@gmail.com>
2022-04-13 15:33:42 +0700

    currency

---
+++ src/md2/currency.java
@@ -0,0 +1,15 @@
+package md2;
+
+import java.util.Scanner;
+
+public class currency {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời bạn nhập vào số tiền USD: ");
+        usd = sc.nextDouble();
+        double quydoi = usd*23000;
+        System.out.print("Giá trị VND:" +quydoi);
+    }
+}
