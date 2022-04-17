cd07a083476889d18479f25bf3a56397435ec749
huynhvinh8398 <huynhvanvinh080398@gmail.com>
2022-04-17 15:54:04 +0700

    smaillvaluearray

---
+++ src/md2/array/smallValueArray.java
@@ -0,0 +1,18 @@
+package md2.array;
+
+public class smallValueArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 8, 2, 15, 3, 4};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++ ) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
